d249a961187cb3754099c099280e2490fb218fb7
Mixuttih <alanhommis@gmail.com>
2025-09-03 11:12:34 +0300

    tehtävät tehty

--- mod08/tehtava2.py
+++ mod08/tehtava2.py
@@ -23,6 +23,8 @@ def tietokantahaku(i):
     medium_airports = 0
     large_airports = 0
     closed = 0
+    seaplane_bases = 0
+    balloonports = 0
 
     if kursori.rowcount > 0:
         for rivi in tulos:
@@ -36,12 +38,26 @@ def tietokantahaku(i):
                 large_airports += 1
             elif rivi[0] == "closed":
                 closed += 1
+            elif rivi[0] == "seaplane_base":
+                seaplane_bases += 1
+            elif rivi[0] == "balloonport":
+                balloonports += 1
+
     print(f"Maassa {i} on:")
-    print(f"{heliports} helikopterikenttää")
-    print(f"{small_airports} pientä lentokenttää")
-    print(f"{medium_airports} keskikokoista lentokenttää")
-    print(f"{large_airports} isoa lentokenttää")
-    print(f"{closed} suljettua lentokenttää.")
+    if heliports > 0:
+        print(f"{heliports} helikopterikenttää")
+    if small_airports > 0:
+        print(f"{small_airports} pientä lentokenttää")
+    if medium_airports > 0:
+        print(f"{medium_airports} keskikokoista lentokenttää")
+    if large_airports > 0:
+        print(f"{large_airports} isoa lentokenttää")
+    if seaplane_bases > 0:
+        print(f"{seaplane_bases} vesilentokonekenttää")
+    if balloonports > 0:
+        print(f"{balloonports} kuumailmapallokenttää")
+    if closed > 0:
+        print(f"{closed} suljettua lentokenttää.")
     return
 
 koodi = input("Syötä maa-koodi: ")
