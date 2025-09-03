4b14a0c304add514626cc14174e5acbc9d8a2551
Mixuttih <alanhommis@gmail.com>
2025-09-03 10:06:14 +0300

    tehtävät tehty

--- mod07/tehtavat1-3.py
+++ mod07/tehtavat1-3.py
@@ -1,10 +1,10 @@
-#1
+#1: vuodenaika kuukauden numerosta
 vuodenajat = ("talvi", "talvi", "kevät", "kevät", "kevät", "kesä", "kesä", "kesä", "syksy", "syksy", "syksy", "talvi")
 kuukausi = input("Anna kuukauden järjestysnumero (1-12): ")
 vuodenaika = vuodenajat[int(kuukausi)-1]
 print (f"{kuukausi}. kuukausi on {vuodenaika}.")
 
-#2
+#2: nimilista johon lisätään nimiä ja tulostetaan lopuksi
 nimilista = {""} #Set ilman arvoja on dictionary
 nimilista.remove("") #Poistetaan ylimääräinen arvo
 
@@ -22,7 +22,7 @@ while nimi != "":
 for i in nimilista:
     print(i)
 
-#3
+#3: lentokenttien haku- ja syöttöjärjestelmä
 lentokentat = {
     "EFHK":"Helsinki-Vantaa Airport",
     "EFHF":"Helsinki-Malmi Airport",
@@ -34,20 +34,27 @@ print("Kirjoita [HAE] jos haluat hakea lentokentän tietoja.")
 print("Kirjoita [LOPETA] jos haluat lopettaa")
 tilanne = input("[SYÖTÄ][HAE][LOPETA]: ")
 
-
+#Prosessi joka pyörii kunnes käyttäjä antaa komennon "lopeta"
 while tilanne.upper() != "LOPETA":
+    #Prosessissa on 2 tilaa, syöttö ja haku
     if tilanne.upper() == "SYÖTÄ":
         kentan_nimi = input("Syötä uuden lentokentän nimi: ")
-        kentan_koodi = input("Syötä uuden lentokentän ICAO -koodi: ")
+        kentan_koodi = input("Syötä uuden lentokentän ICAO -koodi: ").upper()
         lentokentat[kentan_koodi] = kentan_nimi
+        #Käyttäjä voi vaihtaa prosessin tilaa
         tilanne = input("[SYÖTÄ][HAE][LOPETA]: ")
     if tilanne.upper() == "HAE":
-        kentan_koodi = input("Syötä haettavan lentokentän ICAO -koodi: ")
+        kentan_koodi = input("Syötä haettavan lentokentän ICAO -koodi: ").upper()
+        #Etsitään syötettyä koodia
         if kentan_koodi in lentokentat:
-            print(f"{kentan_koodi} on {lentokentat[kentan_koodi]}.")
+            print(f"{kentan_koodi} -koodin lentokenttä on {lentokentat[kentan_koodi]}.")
             tilanne = input("[SYÖTÄ][HAE][LOPETA]: ")
         else:
             print("Haettua lentokenttää ei löydy")
             tilanne = input("[SYÖTÄ][HAE][LOPETA]: ")
-
+    #Jos käyttäjä ei syötä oikeaa toimintoa
+    else:
+        print("Virhe! Syötettyä toimintoa ei ole.")
+        tilanne = input("[SYÖTÄ][HAE][LOPETA]: ")
+#Prosessi lopetettu
 print("Ohjelma lopetettu.")
