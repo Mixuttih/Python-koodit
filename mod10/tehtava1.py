01301b3a661df777fbdea9a4c7389683c43b614c
Mixuttih <alanhommis@gmail.com>
2025-10-21 09:32:34 +0300

    tehtävät tehty

---
+++ mod10/tehtava1.py
@@ -0,0 +1,47 @@
+class Hissi():
+    def __init__(self):
+        self.alin_kerros = 1
+        self.ylin_kerros = 10
+        self.kerros = 1
+
+    def siirry_kerrokseen(self, siirtyma):
+        #Tarkastetaan mennäänkö ylös
+        if siirtyma > self.kerros:
+            #Looppi kunnes kerros on saavutettu
+            while siirtyma > self.kerros:
+                #Mennään kerros ylös
+                self.siirry_ylos()
+                print(f"Siirrytty kerrokseen {self.kerros}")
+            #Kun kerros saavutettu
+            print(f"Kerros {self.kerros} saavutettu")
+
+        #Tarkastetaan mennäänkö alas
+        elif siirtyma < self.kerros:
+            #Looppi kunnes kerros saavutettu
+            while siirtyma < self.kerros:
+                #Mennään kerros alas
+                self.siirry_alas()
+                print(f"Siirrytty kerrokseen {self.kerros}")
+            #Kun kerros saavutettu
+            print(f"Kerros {self.kerros} saavutettu")
+
+        #Jos kerros on sama
+        else:
+            print(f"Kerros {self.kerros} saavutettu")
+
+    def siirry_ylos(self):
+        #Nostetaan kerrosta
+        self.kerros += 1
+
+    def siirry_alas(self):
+        #Lasketaan kerrosta
+        self.kerros += -1
+
+#Luodaan olio
+hissi = Hissi()
+
+#Nostetaan hissi ylös
+hissi.siirry_kerrokseen(10)
+
+#Lasketaan hissi alas
+hissi.siirry_kerrokseen(1)
