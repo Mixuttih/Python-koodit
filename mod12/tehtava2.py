d9b61def48998c69c5fc6166e9c6001f091bf5d9
Mixuttih <alanhommis@gmail.com>
2025-10-28 09:50:11 +0200

    tehtävät tehty

--- mod12/tehtava2.py
+++ mod12/tehtava2.py
@@ -1,15 +1,28 @@
 import requests
-import json
 
+#Loopin muuttuja
 saakone = True
 api = "8aaee5bfe760d365fe7df353fce3e964"
 
+#Loputon loop
 while saakone == True:
+    #Paikkakunta jonka sää haetaan
     hakusana = input("Type a location for weather information: ")
 
+    #Haetaan paikan sijainti
     paikkahaku = requests.get(f"https://api.openweathermap.org/geo/1.0/direct?q={hakusana}&appid={api}").json()
+
+    #Haetaan sijainnin perusteella sää
     haku = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={paikkahaku[0]["lat"]}&lon={paikkahaku[0]["lon"]}&appid={api}").json()
+
+    #Printataan säätiedot
     print("Weather information:")
+
+    #Sään tekstikuvaus
     print(haku["weather"][0]["main"])
+
+    #Asetetaan lämpötila muuttujaan
     kelvin = haku["main"]["temp"]
+
+    #Muutetaan lämpötila Celsius-asteiksi ja printataan
     print(f"{round(kelvin - 273.15)}°C")
