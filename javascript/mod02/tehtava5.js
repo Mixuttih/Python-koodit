95d4e9e6ddadf9818fefb79b31f31d3b612bf8e1
Mixuttih <alanhommis@gmail.com>
2025-11-04 10:58:39 +0200

    tehtävät aloitettu

--- javascript/mod02/tehtava5.js
+++ javascript/mod02/tehtava5.js
@@ -4,11 +4,14 @@ let taasUusiNumero = window.prompt("Syötä numero:")
 while (uusiNumeroArray.includes(parseInt(taasUusiNumero)) === false) {
 
     //Jos käyttäjä syöttää jotain muuta kuin numeron
-    //TAI
-    //Jos käyttäjä yrittää syöttää numeron joka on jo listassa, kun kysytään numeroa uudelleen
-       while (isNaN(parseInt(taasUusiNumero)) || uusiNumeroArray.includes(parseInt(taasUusiNumero)) === true) {
-           taasUusiNumero = window.prompt("Syötä oikea numero:")
-       }
+   while (isNaN(parseInt(taasUusiNumero))) {
+       taasUusiNumero = window.prompt("Syötä oikea numero:")
+   }
+
+   //Jos käyttäjä yrittää syöttää numeron joka on jo listassa, kun kysytään numeroa uudelleen
+   if (uusiNumeroArray.includes(parseInt(taasUusiNumero)) === true) {
+       break
+   }
 
     uusiNumeroArray.unshift(parseInt(taasUusiNumero))
     taasUusiNumero = window.prompt("Syötä numero:")
