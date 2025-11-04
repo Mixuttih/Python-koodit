4c42e44ccd2fe48d2bc1431cbe2701d6a388bdc3
Mixuttih <alanhommis@gmail.com>
2025-11-04 10:05:18 +0200

    tehtävät aloitettu

---
+++ javascript/mod02/tehtava3.js
@@ -0,0 +1,13 @@
+let koiraLista = []
+
+for (let i = 1; i < 7; i++) {
+    let koiraNimi = window.prompt(`Syötä koiran ${i}. nimi:`)
+    koiraLista.push(koiraNimi)
+}
+
+koiraLista.sort()
+
+for (let i = 0; i < 6; i++) {
+    document.getElementById("koiralista").innerHTML += `<li>${koiraLista[koiraLista.length - 1]}</li>`
+    koiraLista.pop()
+}
